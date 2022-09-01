ad305c74b99c19339e7606811694e2cd4bb3c08c
jill bhatt <bhattjill@gmail.com>
2022-09-02 00:22:29 +0530

    Changed nginx config #2

--- nginx/conf.d
+++ nginx/conf.d
@@ -1,7 +1,10 @@
-server {
-    listen 5000;
+upstream my-app {
+    server http://localhost:5000 weight=1;
+    server http://localhost:5000 weight=1;
+}
 
+server {
     location / {
-        proxy_pass http://server:5000;
+        proxy_pass http://my-app;
     }
 }
