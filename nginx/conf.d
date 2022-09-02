898e921bf4e22d75a7dc10eb0bb6c78e78e22166
jill bhatt <bhattjill@gmail.com>
2022-09-02 11:15:28 +0530

    Changed the nginx configs #1

--- nginx/conf.d
+++ nginx/conf.d
@@ -1,16 +1,26 @@
-server {
-    listen 5000;
-
-    location / {
-        proxy_pass http://server:5000;
-
-        proxy_http_version 1.1;
-        proxy_set_header Upgrade $http_upgrade;
-        proxy_set_header Connection 'upgrade';
-        proxy_set_header Host $host;
-        proxy_cache_bypass $http_upgrade;
-
-        # cookies
-        proxy_cookie_path / "/; HTTPOnly;";
+http {
+    upstream apiServers {
+        server localhost:5001;
+        server localhost:5002;
+        server localhost:5003;
     }
+
+    server {
+        listen 5000;
+
+        location / {
+            proxy_pass http://apiServers;
+
+            proxy_http_version 1.1;
+            proxy_set_header Upgrade $http_upgrade;
+            proxy_set_header Connection 'upgrade';
+            proxy_set_header Host $host;
+            proxy_cache_bypass $http_upgrade;
+
+            # cookies
+            proxy_cookie_path / "/; HTTPOnly;";
+        }
+    }
+
+    events {}
 }
